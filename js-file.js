6aece7100817226c56d68e7fb26fc0fb2f80f94a
rlndgg <rlndgg@gmail.com>
2022-03-01 10:38:51 -0600

    changing script to .js file

---
+++ js-file.js
@@ -0,0 +1,61 @@
+var moveArray = ["rock","paper","scissors"]
+
+    function computerPlay(){
+    
+        let arrayIndex =  Math.floor(Math.random()*3);
+    
+        return arrayIndex
+}
+    
+    function playRound(playerSelection,computerSelection){
+        
+        let result
+
+        if (playerSelection === computerSelection) {
+            result = ["You tied",0];
+            return result;
+        }
+        else if ((playerSelection + 1) % 3  === computerSelection) {
+            result = [`You lose, ${moveArray[computerSelection]} beats ${moveArray[playerSelection]}`,-1];
+            return result;
+        }   
+        else {
+            result = [`You win, ${moveArray[playerSelection]} beats ${moveArray[computerSelection]}`,1];
+            return result;
+        }
+
+    }
+    let computerSelection
+    let playerSelection
+    let computerScore = 0
+    let playerScore = 0
+
+    function game(){
+
+        let resultArray
+        let playerStr
+
+        while (Math.max(playerScore,computerScore)<5) {
+            computerSelection = computerPlay();
+            playerStr = window.prompt("Choose: Rock, Paper or Scissors?");
+            playerSelection = moveArray.indexOf(playerStr.toLowerCase());
+            
+
+            console.log(playerSelection);
+            console.log(computerSelection);
+            resultArray = playRound(playerSelection,computerSelection);
+
+            if (resultArray[1] >= 0) {
+                playerScore = playerScore + resultArray[1];
+                }
+            else {
+                computerScore = computerScore - resultArray[1]
+            }
+            
+            console.log(resultArray[0]);
+            console.log(playerScore);
+            console.log(computerScore);
+        }        
+    }
+
+    game();
